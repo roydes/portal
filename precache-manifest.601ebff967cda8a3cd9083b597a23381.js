14962e8737113730cc10414edd69b159bae5165d
Roides Javier Cruz Lara <roidescruzlara@gmail.com>
2019-03-27 17:41:02 -0600

    Updates

---
+++ precache-manifest.601ebff967cda8a3cd9083b597a23381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e00cabc6597aa65c75631e473712d83",
+    "url": "/static/media/profile.0e00cabc.jpg"
+  },
+  {
+    "revision": "bf339332f81181310b365c0a179feee0",
+    "url": "/static/media/background_dark.bf339332.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a1506acdd5c69e06ac6",
+    "url": "/static/js/main.d7299409.chunk.js"
+  },
+  {
+    "revision": "2a0f63a52f6cf9028db4",
+    "url": "/static/js/2.8da2027e.chunk.js"
+  },
+  {
+    "revision": "6a1506acdd5c69e06ac6",
+    "url": "/static/css/main.3dc8d8d9.chunk.css"
+  },
+  {
+    "revision": "69a8abd42c9df162b889b1a1d1c2c1cf",
+    "url": "/index.html"
+  }
+];
