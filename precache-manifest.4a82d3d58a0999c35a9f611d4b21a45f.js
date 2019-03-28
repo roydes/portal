c03b4e87f0d4b00253cc779e8d9d6ddd12f403d7
Roides Javier Cruz Lara <roidescruzlara@gmail.com>
2019-03-27 18:11:26 -0600

    Updates

---
+++ precache-manifest.4a82d3d58a0999c35a9f611d4b21a45f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e00cabc6597aa65c75631e473712d83",
+    "url": "/portal/static/media/profile.0e00cabc.jpg"
+  },
+  {
+    "revision": "bf339332f81181310b365c0a179feee0",
+    "url": "/portal/static/media/background_dark.bf339332.jpg"
+  },
+  {
+    "revision": "05d4c05c31e6e52e5e67",
+    "url": "/portal/static/js/runtime~main.3c49a560.js"
+  },
+  {
+    "revision": "af8132b20a7966cb84ad",
+    "url": "/portal/static/js/main.6d21402b.chunk.js"
+  },
+  {
+    "revision": "2a0f63a52f6cf9028db4",
+    "url": "/portal/static/js/2.8da2027e.chunk.js"
+  },
+  {
+    "revision": "af8132b20a7966cb84ad",
+    "url": "/portal/static/css/main.7a4545c3.chunk.css"
+  },
+  {
+    "revision": "c1fc53c73d6db537e26433148c245c37",
+    "url": "/portal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.601ebff967cda8a3cd9083b597a23381.js"
+  "/portal/precache-manifest.4a82d3d58a0999c35a9f611d4b21a45f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
